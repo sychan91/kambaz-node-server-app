e97d81f81345e477415b4c8ddcd0b78522debe32
sychan91 <c.saiyin@gmail.com>
2025-03-29 17:10:54 -0400

    s change

--- index.js
+++ index.js
@@ -12,23 +12,26 @@ import EnrollmentRoutes from './Kambaz/Enrollments/routes.js';
 
 const app = express();
 
-app.use(cors({
-    credentials: true,
-    origin: process.env.NETLIFY_URL || "http://localhost:5173",
-}));
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.NETLIFY_URL || "http://localhost:5173",
+    })
+);
 
 const sessionOptions = {
-    secret: process.env.SESSION_SECRET || 'kambaz',
+    secret: process.env.SESSION_SECRET || "kambaz",
     resave: false,
     saveUninitialized: false,
 };
 if (process.env.NODE_ENV === "development") {
+    console.log("here")
     sessionOptions.proxy = true;
     sessionOptions.cookie = {
-        sameSite: 'none',
+        sameSite: "none",
         secure: true,
         domain: process.env.NODE_SERVER_DOMAIN,
-    };
+    };
 }
 app.use(session(sessionOptions));
 app.use(express.json());
