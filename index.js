7e886816d62c3b1e84747e3d4021d5808c2b2cf9
sychan91 <c.saiyin@gmail.com>
2025-04-05 00:26:57 -0400

    6.2.4 complete

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import cors from 'cors';
@@ -10,6 +11,8 @@ import ModuleRoutes from './Kambaz/Modules/routes.js';
 import AssignmentRoutes from './Kambaz/Assignments/routes.js';
 import EnrollmentRoutes from './Kambaz/Enrollments/routes.js';
 
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 
 app.use(
