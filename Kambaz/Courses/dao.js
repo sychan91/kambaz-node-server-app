c71d4731bd289355eba193d07a3d433de7a9572c
sychan91 <c.saiyin@gmail.com>
2025-04-06 15:25:11 -0400

    6.4.1.4 complete

--- Kambaz/Courses/dao.js
+++ Kambaz/Courses/dao.js
@@ -1,31 +1,45 @@
-import db from "../Database/index.js";
+// import db from "../Database/index.js";
+import model from "./model.js";
+import enrollmentModel from "../Enrollments/model.js";
 import {v4 as uuidv4} from "uuid";
 
-export function findAllCourses() {
-  return db.courses;
+export async function findAllCourses() {
+  const courses = await model.find();
+  return courses;
 }
 
-export function findCoursesForEnrolledUser(userId) {
-  const {courses, enrollments} = db;
-  const enrolledCourses = courses.filter((course) => enrollments.some((enrollment) => enrollment.user === userId && enrollment.course === course._id));
-  return enrolledCourses;
+export async function findCoursesForEnrolledUser(userId) {
+  // const {courses, enrollments} = db;
+  // const enrolledCourses = courses.filter((course) => enrollments.some((enrollment) => enrollment.user === userId && enrollment.course === course._id));
+  // return enrolledCourses;
+  // const enrollments = await enrollmentModel.find({user: String(userId)});
+  const all = await enrollmentModel.find({});
+
+
+const enrollments = all.filter(e => String(e.user) === String(userId));
+  const courseIds = enrollments.map(e=> e.course);
+  const courses = await model.find({_id: {$in: courseIds}});
+  return courses;
 }
 
-export function createCourse(course) {
+export async function createCourse(course) {
   const newCourse = {...course, _id: uuidv4()};
-  db.courses = [...db.courses, newCourse];
-  return newCourse;
+  return model.create(newCourse);
 }
 
-export function deleteCourse(courseId) {
-  const {courses, enrollments} = db;
-  db.courses = courses.filter((course) => course._id !== courseId);
-  db.enrollments = enrollments.filter((enrollment) => enrollment.course !== courseId);
+export async function deleteCourse(courseId) {
+  // const {courses, enrollments} = db;
+  // db.courses = courses.filter((course) => course._id !== courseId);
+  // db.enrollments = enrollments.filter((enrollment) => enrollment.course !== courseId);
+  const status = await model.deleteOne({_id: courseId});
+  return status;
 }
 
-export function updateCourse(courseId, courseUpdates) {
-  const {courses} = db;
-  const course = courses.find((course) => course._id === courseId);
-  Object.assign(course, courseUpdates);
-  return course;
+export async function updateCourse(courseId, courseUpdates) {
+  // const {courses} = db;
+  // const course = courses.find((course) => course._id === courseId);
+  // Object.assign(course, courseUpdates);
+  // return course;
+  const status = await model.updateOne({_id: courseId}, {$set: courseUpdates});
+  return status;
 }
---
+++ Kambaz/Courses/schema.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const courseSchema = new mongoose.Schema({
+    _id: String,
+    name: String,
+    number: String,
+    startDate: Date,
+    endDate: Date,
+    department: String,
+    credits: Number,
+    description: String,
+},
+{collection: 'courses'});
+
+export default courseSchema;
