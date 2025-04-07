47c049670f37294d2d4dee168857203816982f17
sychan91 <c.saiyin@gmail.com>
2025-04-06 22:05:07 -0400

    6.4.3.3 enrolling/unenrolling complete

--- Kambaz/Enrollments/dao.js
+++ Kambaz/Enrollments/dao.js
@@ -1,21 +1,31 @@
 import db from "../Database/index.js";
+import model from "./model.js";
 
-export function enrollUserInCourse(userId, courseId) {
-    const enrollment = { user: userId, course: courseId };
-    db.enrollments.push(enrollment);
-    return enrollment;
+export function enrollUserInCourse(user, course) {
+    // const enrollment = { user: userId, course: courseId };
+    // db.enrollments.push(enrollment);
+    // return enrollment;
+    return model.create({user, course, _id: `${user}-${course}`});
   }
   
-  export function unenrollUserFromCourse(userId, courseId) {
-    db.enrollments = db.enrollments.filter(
-      (e) => !(e.user === userId && e.course === courseId)
-    );
-    return { status: "unenrolled" };
+  export function unenrollUserFromCourse(user, course) {
+    // db.enrollments = db.enrollments.filter(
+    //   (e) => !(e.user === userId && e.course === courseId)
+    // );
+    // return { status: "unenrolled" };
+    return model.deleteOne({user, course});
   }
   
-  export function findCoursesForUser(userId) {
-    const { courses, enrollments } = db;
-    return courses.filter((course) =>
-      enrollments.some((e) => e.user === userId && e.course === course._id)
-    );
+  export async function findCoursesForUser(userId) {
+    // const { courses, enrollments } = db;
+    // return courses.filter((course) =>
+    //   enrollments.some((e) => e.user === userId && e.course === course._id)
+    // );
+    const enrollments = await model.find({user: userId}).populate('course');
+    return enrollments.map((enrollment) => enrollment.course);
+  }
+
+  export async function findUsersForCourse(courseId) {
+    const enrollments = await model.find({course: courseId}).populate('user');
+    return enrollments.map((enrollment) => enrollment.user);
   }
--- Kambaz/Users/routes.js
+++ Kambaz/Users/routes.js
@@ -103,6 +103,46 @@ export default function UserRoutes(app) {
         res.json(newCourse);
       };
 
+      const findCoursesForUser = async (req, res) => {
+        const currentUser = req.session['currentUser'];
+        if (!currentUser) {
+          res.sendStatus(401);
+          return;
+        }
+        if (currentUser.role === "ADMIN") {
+          const courses = await courseDao.findAllCourses();
+          res.json(courses);
+          return;
+        }
+        let {uid} = req.params;
+        if (uid === 'current') {
+          uid = currentUser._id;
+        }
+        const courses = await enrollmentsDao.findCoursesForUser(uid);
+        res.json(courses);
+      };
+
+      const enrollUserInCourse = async (req, res) => {
+        let {uid, cid} = req.params;
+        if (uid === 'current') {
+          const currentUser = req.session['currentUser'];
+          uid = currentUser._id;
+        }
+        const status = await enrollmentsDao.enrollUserInCourse(uid, cid);
+        res.send(status);
+      };
+
+      const unenrollUserFromCourse = async (req, res) => {
+        let {uid, cid} = req.params;
+        if (uid === 'current') {
+        const currentUser = req.session['currentUser'];
+        uid = currentUser._id;
+      }
+      const status = await enrollmentsDao.unenrollUserFromCourse(uid, cid);
+      res.send(status);
+      };
+
+
       // app.get("/api/courses/:cid/users", (req, res) => {
       //   const { cid } = req.params;
       //   const enrolledUserIds = db.enrollments
@@ -128,6 +168,10 @@ export default function UserRoutes(app) {
     app.post("/api/users/signout", signout);
     app.post("/api/users/profile", profile);
     app.get('/api/users/:userId/courses', findCoursesForEnrolledUser);
+    app.post('/api/users/:uid/courses/:cid', enrollUserInCourse);
+    app.delete('/api/users/:uid/courses/:cid', unenrollUserFromCourse);
+    app.get('/api/users/:uid/courses', findCoursesForUser);
     app.post('/api/users/current/courses', createCourse);
+    
 
 }
